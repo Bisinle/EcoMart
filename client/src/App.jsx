26454f93601a90ae0f462ba59d8d87fbb2e848c6
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-10-03 15:22:07 +0300

    created Header, footer, navbar, product card & search components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,19 @@
+import Navbar from "./components/Navbar";
+import ProductCard from "./components/ProductCard";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import SearchFilter from "./components/SearchFilter";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Header />
+
+      <ProductCard />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
