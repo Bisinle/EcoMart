36f8b29dbcbdb7ccbc3a71796d5beb0c941869ef
arnold-aswan <arnold.aswani@student.moringaschool.com>
2023-10-04 21:46:17 +0300

    added components to be rendered

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,14 @@
 import Navbar from "./components/Navbar";
-import ProductCard from "./components/ProductCard";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import SearchFilter from "./components/SearchFilter";
+import ProductList from "./components/ProductList";
 
 function App() {
   return (
     <>
       <Navbar />
       <Header />
-
-      <ProductCard />
+      <ProductList />
       <Footer />
     </>
   );
